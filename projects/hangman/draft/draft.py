70c95c1c9618f23846fe98ab94bc09a5c607164e
Loic Rouaud <loic.rouaud@gmail.com>
2024-06-17 23:09:16 +0200

    projects sauv

--- projects/hangman/draft/draft.py
+++ projects/hangman/draft/draft.py
@@ -1,28 +1,28 @@
-def get_word():
+def get_word() -> str:
     word = input("Enter your word: ")
     print(f"Your word is {word}.")
     return word
 
-def get_lives():
+def get_lives() -> int:
     live_numbers = int(input("How many lives do you want? "))
     print(f"You have {live_numbers} lives")
     return live_numbers
 
 def get_guess(suggested_letters):
-    suggested_letters  = suggested_letters.append(input("Enter a letter to guess: "))
+    letter_guess.append(suggested_letters)
     return suggested_letters
+
+
+
+
+
+######################################""""
 
 
 # test of the functions
 get_word()
 get_lives()
-get_guess()
-
-
-
-# def assess_guess(secret_word, guessed_letter, lives_left):
-
-
-# def display_word(secret_word, suggested_letters):
+letter_guess = input(f"Can you suggested a letter? ")
+get_guess(letter_guess)
 
 

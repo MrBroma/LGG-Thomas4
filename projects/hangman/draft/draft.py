a43d746a420a3574976f3d82f32612de8929fa40
Loic Rouaud <loic.rouaud@gmail.com>
2024-06-18 09:57:15 +0200

    hangman project

--- projects/hangman/draft/draft.py
+++ projects/hangman/draft/draft.py
@@ -3,14 +3,37 @@ def get_word() -> str:
     print(f"Your word is {word}.")
     return word
 
+
 def get_lives() -> int:
     live_numbers = int(input("How many lives do you want? "))
     print(f"You have {live_numbers} lives")
     return live_numbers
 
+
 def get_guess(suggested_letters):
-    letter_guess.append(suggested_letters)
-    return suggested_letters
+    guess = input("Enter a letter to guess: ")
+    suggested_letters.append(guess)
+    print(suggested_letters)
+    return guess
+
+
+# def asses_guess(secret_word, guessed_letter, lives_left):
+
+
+# def display_word(secret_word, suggested_letters):
+
+
+# Start of the program
+if __name__ == "__main__":
+    # variable for the program
+    suggested_letters = []
+    
+    # call of the functions
+    get_word()
+    get_lives()
+    get_guess(suggested_letters)
+
+
 
 
 
@@ -20,9 +43,11 @@ def get_guess(suggested_letters):
 
 
 # test of the functions
-get_word()
-get_lives()
-letter_guess = input(f"Can you suggested a letter? ")
-get_guess(letter_guess)
+# get_word()
+# get_lives()
 
 
+# user_letter = ['a', 'f', 'k', 'p']
+# user_guess = get_guess(user_letter)
+
+# print(user_letter)

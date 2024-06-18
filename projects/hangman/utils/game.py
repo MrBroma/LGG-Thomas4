809d376cc13446efec72dcfdbf6ed49e71e9a3df
Loic Rouaud <loic.rouaud@gmail.com>
2024-06-18 16:41:40 +0200

    hangman project suite

---
+++ projects/hangman/utils/game.py
@@ -0,0 +1,17 @@
+from wcwidth import list_versions
+
+
+class Hangman:
+
+    def __init__(self) -> None:
+        pass
+
+
+    possible_words = ['becode', 'learning', 'mathematics', 'sessions']
+
+    word_to_find = 
+
+    
+    correctly_guessed_letters
+
+    

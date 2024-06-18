fe6ea7eccc9ffbe3494af32d8af5ab98fe4501da
Loic Rouaud <loic.rouaud@gmail.com>
2024-06-18 16:31:48 +0200

    hangman project

--- projects/hangman/draft/draft.py
+++ projects/hangman/draft/draft.py
@@ -1,5 +1,7 @@
+from typing import List
+
 def get_word() -> str:
-    word = input("Enter your word: ")
+    word = input("Enter your word to be guessed: ")
     print(f"Your word is {word}.")
     return word
 
@@ -10,44 +12,50 @@ def get_lives() -> int:
     return live_numbers
 
 
-def get_guess(suggested_letters):
-    guess = input("Enter a letter to guess: ")
-    suggested_letters.append(guess)
-    print(suggested_letters)
-    return guess
+def get_guess(suggested_letters: List[str]) -> List[str]:
+    guess_letter = input("Enter a letter to guess: ")
+    while not guess_letter.isalpha() or len(guess_letter) != 1 or guess_letter in suggested_letters:
+        if not guess_letter.isalpha():
+            guess_letter = input("This is not a letter\n Enter a letter to guess: ")
+        elif len(guess_letter) != 1:
+            guess_letter = input("The char is too long\n Enter a letter to guess: ")
+        elif guess_letter in suggested_letters:
+            guess_letter = input("The char is already in the list\n Enter a letter to guess: ")
+        else:
+            print("Error")
+    suggested_letters.append(guess_letter)
+    return(suggested_letters)
 
 
-# def asses_guess(secret_word, guessed_letter, lives_left):
+def asses_guess(secret_word: str, guessed_letter: str, lives_left: int) -> int:
+    if guessed_letter not in secret_word:
+        lives_left -= 1
+    return lives_left
 
 
-# def display_word(secret_word, suggested_letters):
-
+def display_word(secret_word: str, suggested_letters: str): 
+    hidden_word = list("_" * len(secret_word))
+    for i, letter in enumerate(secret_word):
+        if suggested_letters != "_" and suggested_letters == letter:
+            hidden_word[i] = letter
+    print(' '.join(hidden_word))
+    
 
 # Start of the program
 if __name__ == "__main__":
-    # variable for the program
-    suggested_letters = []
+    game = True
+    # define the secret word with the function get_word()
+    secret_word = get_word().upper()
+    # choose the number of lives of the player
+    number_lives = get_lives()
     
-    # call of the functions
-    get_word()
-    get_lives()
-    get_guess(suggested_letters)
+    while game:
+        # asking the player to guess a letter assign to suggested letter
+        guessed_letter = ''.join(get_guess([])).upper()
 
+        # check if the guess is correct and return number of lives
+        lives_left = asses_guess(secret_word, guessed_letter, number_lives)
 
+        # display the secret word hidden with hiphen
+        display_word(secret_word, guessed_letter)
 
-
-
-
-
-######################################""""
-
-
-# test of the functions
-# get_word()
-# get_lives()
-
-
-# user_letter = ['a', 'f', 'k', 'p']
-# user_guess = get_guess(user_letter)
-
-# print(user_letter)
